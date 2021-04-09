bb90d938745a9fc810ee68fff42a23fcf7ed952f
Stefan Bethke <stb@lassitu.de>
2021-04-09 16:17:39 +0200

    use setuptools_scm to get version number

--- setup.py
+++ setup.py
@@ -2,21 +2,13 @@ import os
 import setuptools
 import subprocess
 
-def get_version():
-    if "GITHUB_REF" in os.environ and os.environ["GITHUB_REF"].startswith("refs/tags/"):
-        return os.environ["GITHUB_REF"][len("refs/tags/"):]
-    try:
-        return subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"]).decode("UTF-8").strip()
-    except Exception as e:
-        return "0.9.0"
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
 setuptools.setup(
     name="obs-scene-transporter",
-    version=get_version(),
+    use_scm_version=True,
     author="Stefan Bethke",
     author_email="stb@lassitu.de",
     description="Import and export OBS Studio scenes including all assets",
@@ -37,4 +29,5 @@ setuptools.setup(
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
     python_requires=">=3.6",
+    setup_requires=['setuptools_scm'],
 )
